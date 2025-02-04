1c0753c9804f207a36fafbf69c147ae79361702c
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-02-04 19:28:29 +0700

    SEO

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 const btn = document.getElementById("topBtn");
+const menu = document.getElementById("menu");
 
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 0) {
-        btn.classList.add('show');
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
+  }
+});
+
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("opened");
+    menu.setAttribute("aria-expanded", menu.classList.contains("opened"));
+    if (menu.classList.contains("opened")) {
+      dropList.style.maxHeight = "300px";
+      dropList.style.opacity = "1"; 
+      dropList.style.display = "flex";
     } else {
-        btn.classList.remove('show');
+      dropList.style.maxHeight = "0px";
+      dropList.style.opacity = "0";
+      
+      setTimeout(() => {
+        dropList.style.display = "none";
+      }, 500);
     }
-});
-
-btn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
+  });
